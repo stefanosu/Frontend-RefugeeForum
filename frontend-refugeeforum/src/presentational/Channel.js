006320903bf1b4f85d3e2b3c2fd8cacc742e1311
Stefanos <stefanosu422@gmail.com>
2020-03-23 21:05:15 -0400

    modified addNewChannel method

--- frontend-refugeeforum/src/presentational/Channel.js
+++ frontend-refugeeforum/src/presentational/Channel.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux'
 import ChannelCards from './ChannelCards'
 import NewChannelForm from './NewChannelForm';
 
-
 const Channel = ({sessions}) => {
   const renderChannelCards = () => { 
     if(sessions.user){
